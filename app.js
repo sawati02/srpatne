72b9c8bc5228a6410d7447df75965f05eac60768
Iffat-kausar <106217061+Iffat-kausar@users.noreply.github.com>
2023-05-23 14:54:27 +0530

    in app.js create a wrapper

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app=express();
 app.get("/",(req,res)=>{
     res.send("Welcome to my Github")
 })
+app.get("/name",(req,res)=>{
+    res.send(["Swati","Pinky"])
+})
 app.listen(2525,()=>{
     res.send("Server running is port on 2525")
 })
